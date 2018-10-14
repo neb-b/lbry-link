883ee0325788975c5e9b91b078b7385310981c99
Sean Yesmunt <sean@lbry.io>
2018-10-14 19:27:04 -0400

    initial script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lbry-redirect
+# LBRY Redirect
+
+A chrome extension that wraps valid LBRY URLs with an `<a>` tag that links to open.lbry.io/{LBRY_URL}
+
+lbry://SUPERTHOMASBROS (will show as a link with this extension installed)
---
+++ content.js
@@ -0,0 +1,18 @@
+const pageContent = window.document.all[0];
+const innerHTML = pageContent.innerHTML;
+const urlRegex = /(lbry:\/\/)([a-zA-Z0-9-]*)(#[a-zA-Z0-9]*)*/gm;
+
+const newInnerHTML = innerHTML.replace(urlRegex, function(str) {
+
+  const [match, proto, name, claimId] = arguments;
+
+  let href = `https://open.lbry.io/${name}`;
+  if (claimId) {
+    href += claimId
+  }
+
+  return `<a href="${href}">${str}</a>`;
+
+});
+
+pageContent.innerHTML = newInnerHTML
