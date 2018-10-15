eab701c93b27d03714740d05d35ff5b9b4ed14f9
Sean Yesmunt <sean@lbry.io>
2018-10-14 23:04:34 -0400

    blacklist youtube

--- content.js
+++ content.js
@@ -1,19 +1,31 @@
-const htmlNodes = window.document.all;
-let body;
+const currentUrl = window.location.href;
 
-for (var i = 0; i < htmlNodes.length; i ++) {
-  const element = htmlNodes[i];
-  if (element.nodeName === "BODY") {
-    body = element;
-    break;
+// exclude_matches wasn't working for me so I'm just doing it here
+const excludedSites = ['youtube.com'];
+const currentSiteBlackListed = excludedSites.some(blackListedUrl => currentUrl.includes(blackListedUrl));
+
+function addLinks() {
+  const htmlNodes = window.document.all;
+  let body;
+  for (var i = 0; i < htmlNodes.length; i ++) {
+    const element = htmlNodes[i];
+    if (element.nodeName === "BODY") {
+      body = element;
+      break;
+    }
   }
+
+  const urlRegex = /(lbry:\/\/)(@*[a-zA-Z0-9-]*)(#[a-zA-Z0-9]*)*/igm;
+  const innerHTML = body.innerHTML;
+
+  const newInnerHTML = innerHTML.replace(urlRegex, function(str) {
+    return `<a href="${str}">${str}</a>`;
+  });
+
+  body.innerHTML = newInnerHTML;
+
 }
 
-const urlRegex = /(lbry:\/\/)(@*[a-zA-Z0-9-]*)(#[a-zA-Z0-9]*)*/igm;
-const innerHTML = body.innerHTML;
-
-const newInnerHTML = innerHTML.replace(urlRegex, function(str) {
-  return `<a href="${str}">${str}</a>`;
-});
-
-body.innerHTML = newInnerHTML
+if (!currentSiteBlackListed) {
+  addLinks();
+}
