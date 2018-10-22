6ac6b2b2a93e8c131bccfe3af2df69461c5025fd
Sean Yesmunt <sean@lbry.io>
2018-10-22 00:44:04 -0400

    only replace text element instead of entire body

--- content.js
+++ content.js
@@ -1,3 +1,27 @@
+const urlRegex = /(lbry:\/\/)(@*[a-zA-Z0-9-]*)(#[a-zA-Z0-9]*)*/igm;
+
+function searchAndInsert(node, x) {
+  if (node.nodeName == "#text" && node.nodeValue.match(urlRegex)) {
+    // Text with lbry address found
+    // Wrap it in a span so we can alter the innerHTML and add a link
+    const replacementNode = document.createElement('span');
+
+    replacementNode.innerHTML = node.nodeValue.replace(urlRegex, function(str) {
+      return `<a href="${str}">${str}</a>`;
+    });
+
+    node.parentNode.insertBefore(replacementNode, node);
+    node.parentNode.removeChild(node);
+  } else if (node.innerHTML && node.innerHTML.includes('lbry://')) {
+    if (node.childNodes) {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        const currentNode = node.childNodes[i];
+        searchAndInsert(node.childNodes[i], true);
+      }
+    }
+  }
+}
+
 function addLinks() {
   const htmlNodes = window.document.all;
   let body;
@@ -10,13 +34,9 @@ function addLinks() {
     }
   }
 
-  const urlRegex = /(lbry:\/\/)(@*[a-zA-Z0-9-]*)(#[a-zA-Z0-9]*)*/igm;
-  const innerHTML = body.innerHTML;
-  const newInnerHTML = innerHTML.replace(urlRegex, function(str) {
-    return `<a href="${str}">${str}</a>`;
-  });
-
-  body.innerHTML = newInnerHTML;
+  searchAndInsert(body);
 }
 
-addLinks();
+setTimeout(() => {
+  addLinks();
+}, 2000);
